f829afe368f06addf69764b51c766289cf6d95e6
Andreas Martin <andreas.martin@andreasmartin.ch>
2020-02-22 21:01:16 +0100

    Update

--- src/main/java/ch/fhnw/digibp/external/client/SurpriseMenuClient.java
+++ src/main/java/ch/fhnw/digibp/external/client/SurpriseMenuClient.java
@@ -8,6 +8,7 @@ package ch.fhnw.digibp.external.client;
 import org.camunda.bpm.client.ExternalTaskClient;
 import org.camunda.bpm.client.task.ExternalTask;
 import org.camunda.bpm.client.task.ExternalTaskService;
+import org.camunda.bpm.engine.variable.value.TypedValue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -24,10 +25,21 @@ public class SurpriseMenuClient {
     private void subscribeTopics() {
 
         client.subscribe("GetSurpriseMenu")
+                .tenantIdIn("showcase")
                 .handler((ExternalTask externalTask, ExternalTaskService externalTaskService) -> {
                     try {
+                        Boolean vegetarianGuests = false;
+                        TypedValue vegetarianGuestsValue = externalTask.getVariableTyped("vegetarianGuests");
+                        if(vegetarianGuestsValue!=null)
+                            vegetarianGuests = (Boolean) externalTask.getVariableTyped("vegetarianGuests").getValue();
+                        String menu;
+                        if(vegetarianGuests){
+                            menu = Arrays.asList("pizza", "pasta", "verdura").get(new Random().nextInt(3));
+                        } else {
+                            menu = Arrays.asList("pizza", "pasta", "carne", "verdura").get(new Random().nextInt(4));
+                        }
                         Map<String, Object> variables = new HashMap<>();
-                        variables.put("menu", getRandomMenuItem(Arrays.asList("pizza", "pasta", "carne", "verdura")));
+                        variables.put("menu", menu);
 
                         externalTaskService.complete(externalTask, variables);
                     } catch (Exception e) {
@@ -35,10 +47,6 @@ public class SurpriseMenuClient {
                     }
                 })
                 .open();
-    }
-
-    public String getRandomMenuItem(List<String> items) {
-        return items.get(new Random().nextInt(items.size()));
     }
 
 }
